c426f0ec97ad07bad2c504925315b19393539b2b
johncava <jcava@asu.edu>
2017-04-23 16:15:16 -0700

    added max pooling

--- conv_nn.py
+++ conv_nn.py
@@ -24,8 +24,22 @@ def conv2d(matrix,n,m):
     convolve = sigmoid(convolve)
     return convolve
 
+def max_pool(matrix,n,m):
+    max_pool = []
+    for rS in xrange(0, n - 1):
+        c_array = []
+        for cS in xrange(0,m - 1):
+            pool = matrix[rS:rS+2,cS:cS+2]
+            pool_ = np.amax(pool)
+            c_array.append(pool_)
+        max_pool.append(c_array)
+    max_pool = np.matrix(max_pool)
+    return max_pool
 
 def nn():
-    conv2d(input,8,8)
+    convolve1 = conv2d(input,8,8)
+    print convolve1
+    pool1 = max_pool(convolve1,6,6)
+    print pool
 
 nn()
