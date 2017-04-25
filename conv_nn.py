87249336eee1371a1fd64c25eefe08ae5665168d
johncava <jcava@asu.edu>
2017-04-25 00:21:49 -0700

    added more flexibiity for conv2d and max_pool function; added fully connected laters

--- conv_nn.py
+++ conv_nn.py
@@ -1,8 +1,12 @@
 from __future__ import division
 import numpy as np 
 
-input = np.round(np.random.rand(8,8))
-conv1_weight = np.round(np.random.rand(3,3))
+input = np.round(np.random.rand(64,64))
+conv1_weight = np.round(np.random.rand(4,4))
+conv2_weight = np.round(np.random.rand(2,2))
+weights1 = np.random.rand(4,6)
+weights2 = np.random.rand(6,6)
+weights3 = np.random.rand(6,1)
 
 def sigmoid(x):
     return 1/(1 + np.exp(-x))
@@ -10,36 +14,65 @@ def sigmoid(x):
 def sigmoid_derivative(x):
     return x * (1 - x)
 
-def conv2d(matrix,n,m):
+def conv2d(matrix,n,m,filter,weight):
     convolve = []
-    for rS in xrange(0, n - 2):
+    for rS in xrange(0, int(n/filter)):
         c_array = []
-        for cS in xrange(0, m - 2):
-            conv = matrix[rS:rS+3,cS:cS+3]
-            conv_ = np.dot(conv,conv1_weight)
-            sm = np.ndarray.sum(conv_)
+        for cS in xrange(0, int(m/filter)):
+            conv = matrix[(rS*filter):(rS*filter)+filter,(cS*filter):(cS*filter)+filter]
+            conv_ = np.dot(conv,weight)
+            sm = np.ndarray.sum(conv_)/(filter ** 2)
             c_array.append(sm)
         convolve.append(c_array)
-    convolve = np.matrix(convolve)
+    convolve = np.array(convolve)
     convolve = sigmoid(convolve)
     return convolve
 
-def max_pool(matrix,n,m):
+def max_pool(matrix,n,m,p):
     max_pool = []
-    for rS in xrange(0, n - 1):
-        c_array = []
-        for cS in xrange(0,m - 1):
-            pool = matrix[rS:rS+2,cS:cS+2]
+    for rS in xrange(0, int(n/p)):
+        c_array = np.array([])
+        for cS in xrange(0, int(m/p)):
+            pool = matrix[(rS*p):(rS*p)+p,(cS*p):(cS*p)+p]
             pool_ = np.amax(pool)
-            c_array.append(pool_)
+            pool_ = np.array(pool_)
+            c_array = np.append(c_array, pool_)
         max_pool.append(c_array)
-    max_pool = np.matrix(max_pool)
+    max_pool = np.array(max_pool)
     return max_pool
 
 def nn():
-    convolve1 = conv2d(input,8,8)
-    print convolve1
-    pool1 = max_pool(convolve1,6,6)
-    print pool
+    ###
+    #Feedforward
+    ###
+    convolve1 = conv2d(input,64,64,4,conv1_weight)
+    print "convolve1\n", convolve1
+    pool1 = max_pool(convolve1,16,16,2)
+    print "pool1\n",pool1
+    convolve2 = conv2d(pool1, 8,8,2, conv2_weight)
+    print "convolve2\n",convolve2
+    pool2 = max_pool(convolve2, 4,4,2)
+    print "pool2\n" ,pool2
+
+    #Fully Connected Neural Network
+    fc = pool2.flatten()
+    print "fc input layer\n", fc
+
+    hiddenLayer1 = sigmoid(np.dot(fc,weights1))
+    print "fc hidden layer 1\n", hiddenLayer1
+
+    hiddenLayer2 = sigmoid(np.dot(hiddenLayer1, weights2))
+    print "fc hidden layer 2\n", hiddenLayer2
+
+    output = sigmoid(np.dot(hiddenLayer2, weights3))
+    print "output\n", output
+
+    ###
+    #Backpropagation
+    ###
+
+    #unflatten
+    # array.reshape(2,2)
+
 
 nn()
